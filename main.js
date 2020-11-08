bdb4f811255161b79a081d0e2e38ca3765fb8573
Mateusz <matt.borowczyk@gmail.com>
2020-11-08 23:33:14 +0100

    setting up IIFE for animation control

---
+++ main.js
@@ -0,0 +1,19 @@
+const slider = (() => {
+    const activeSlide = (slideID) => {
+
+    }
+    const nextSlide = () => {
+
+    }
+    const previousSlide = () => {
+
+    }
+    const choosenSlide = (slideID) => {
+
+    }
+    return {
+        nextSlide,
+        previousSlide,
+        choosenSlide
+    }
+})();
